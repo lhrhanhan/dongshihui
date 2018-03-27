47d7f7f0adf072e3f98645ec136c10d8a7eb44d6
ouyangyewei <you@example.com>
2018-03-27 14:31:26 +0800

    毕设环境搭建完毕 by 卢焓然

---
+++ my_project/src/components/index/index.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+      11
+    </div>
+</template>
+
+<script>
+    export default {
+      name: "index",
+      data () {
+        return {
+
+        }
+      },
+
+      mounted () {
+        this.$request({
+          type: 'GET',
+          url: 'api/allCtgy?',
+          success: function (res) {
+            console.log(res.data)
+          }
+        })
+      }
+
+    }
+</script>
+
+<style scoped>
+
+</style>
